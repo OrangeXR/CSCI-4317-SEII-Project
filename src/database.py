390e309a59c504fb2844ed7d979b2941d3fe1248
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-25 18:17:43 -0500

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,11 +1,13 @@
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "instance", "astra.db")
 
 def get_db():
-    conn = sqlite3.connect("src/instance/astra.db")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
-
 
 # =============================================================
 # Get User by Username - Returns info for login authentication
