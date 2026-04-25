3def5d919e09738c40e3b8abfb429b4a3ecba58f
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-25 18:09:49 -0500

    Update renderdatabase.py

--- src/renderdatabase.py
+++ src/renderdatabase.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "instance", "astra.db")
 
 def get_db():
-    conn = sqlite3.connect("instance/astra.db")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
